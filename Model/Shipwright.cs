88a8054f5d02044c8561d50c669967ead48072e9
Emil Ordanic <emilordani@gmail.com>
2020-04-29 16:24:03 +0200

    Add Shipwrght class

---
+++ Model/Shipwright.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Shipwright
+    {
+        public Shipwright(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public Fleet CreateFleet(IEnumerable<int> shipLengths)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                Fleet fleet = PlaceShips(shipLengths);
+                if (fleet != null)
+                    return fleet;
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private Fleet PlaceShips(IEnumerable<int> shipLengths)
+        {
+            List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
+
+            Grid grid = new Grid(rows, columns);
+            SquareTerminator terminator = new SquareTerminator(grid);
+            Fleet fleet = new Fleet();
+            Random random = new Random();
+            while (lengths.Count > 0)
+            {
+                var placements = grid.GetAvailablePlacments(lengths[0]);
+                if (placements.Count() == 0)
+                    return null;
+                lengths.RemoveAt(0);
+                int index = random.Next(0, placements.Count());
+                fleet.AddShip(placements.ElementAt(index));
+                var toEliminate = terminator.ToEliminate(placements.ElementAt(index));
+                grid.EliminateSquares(toEliminate);
+            }
+            return fleet;
+        }
+
+        private readonly int rows;
+        private readonly int columns;
+
+    }
+}
