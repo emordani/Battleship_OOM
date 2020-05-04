77451874cae665067991c32de790a96164cee4c3
Emil Ordanic <emilordani@gmail.com>
2020-05-04 18:08:20 +0200

    Implement WhenShipIsSunkenAllSquaresAreMarkedSunken test

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -17,7 +18,11 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         [TestMethod]
         public void WhenShipIsSunkenAllSquaresAreMarkedSunken()
         {
-            throw new NotImplementedException();
+            var ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+            ship.Hit(new Square(1, 4));
+            ship.Hit(new Square(1, 5));
+            var hit = ship.Hit(new Square(1, 6));
+            Assert.AreEqual(HitResult.Sunken, hit);
         }
     }
 }
